7f4dbfc2824d127d635945f4ef632935f62d9d9d
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-05-03 17:24:21 +0200

    Aggiungere i file di progetto.

---
+++ Queue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STACK utilizza il mezzo FIFO
+            Queue<string> prova = new Queue<string>();
+            prova.Enqueue("io");
+            prova.Enqueue("tu");
+            prova.Enqueue("lui");
+            prova.Enqueue("egli");
+            prova.Enqueue("noi");
+
+            //VEDERE
+            Console.WriteLine(prova.Peek()); //vedo primo valore
+
+            //RIMUOVERE
+            prova.Dequeue(); //rimuovo primo
+
+            //ITERAZIONE
+            foreach (string item in prova)
+            {
+                Console.WriteLine(item);
+            }
+
+            //RIPULIRE
+            prova.Clear();
+        }
+    }
+}
